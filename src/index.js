6d26f106b3ef6c88baa7a701eed9e630fd6f6e16
madhavvi <madhavishinde21@gmail.com>
2021-05-13 00:52:27 +0530

    js-and-rockets

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import {prepareData, renderData} from './solution'
+import 'babel-polyfill';
+import { prepareData, renderData } from './solution';
 
-const filterParams = {
-  year: 2018,
-  customerName: 'NASA',
-}
+// const filterParams = {
+//   year: 2018,
+//   customerName: 'NASA',
+// }
 
-// example of run, you could leave it or modify however you want
 fetch('https://api.spacexdata.com/v3/launches/past')
+  // .then(async (response) => await response.json())
   .then(response => response.json())
-  .then(prepareData(filterParams))
-  .then(renderData)
+  .then(results => {
+    const JSONData = prepareData(results);
+    renderData(JSONData);
+  });
--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,40 @@
 // Please implement your solution in this file
+// function to return boolean if customer has a Nasa
+const isNasaCustomer = obj => obj.customers.some(customer => (customer.includes('NASA')));
 
-export const prepareData = () => {}
-export const renderData = () => {}
+export const prepareData = (payload) => (
+  payload.filter(launch => launch.launch_year === '2018')
+  // in inverse chronological order sorting
+  .sort((value1, value2) => {
+    const date1 = new Date(value1.launch_date_utc);
+    const date2 = new Date(value2.launch_date_utc);
+    if (date1 > date2) return -1;
+    if (date1 < date2) return 1;
+    return 0;
+  })
+  //  reduce payloads which do not have Nasa as a customer 
+  .reduce((payloadObj, launch) => {
+    const payloads = launch.rocket.second_stage.payloads;
+    const hasNasaPayload = payloads.some(isNasaCustomer);
+    if (hasNasaPayload) {
+      payloadObj.push({
+        flight_number: launch.flight_number,
+        mission_name: launch.mission_name,
+        payloads_count: payloads.length,
+      });
+    }
+    return payloadObj;
+  }, [])
+  // sorting for payload count
+  .sort((value1, value2) => {
+    if (value1.payloads_count > value2.payloads_count) return -1;
+    if (value1.payloads_count < value2.payloads_count) return 1;
+    return 0;
+  })
+);
+
+// this is to render output
+export const renderData = (data) => {
+  const outputJSON = JSON.stringify(data, null, 2);
+  document.getElementById('out').innerHTML = outputJSON;
+}
